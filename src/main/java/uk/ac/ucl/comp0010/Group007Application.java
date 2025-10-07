e2bdf11b8654253cf89d8cdbe5fa705223de0e88
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-07 16:43:51 +0100

    Fixs the checkstyle issue.

--- src/main/java/uk/ac/ucl/comp0010/Group007Application.java
+++ src/main/java/uk/ac/ucl/comp0010/Group007Application.java
@@ -3,8 +3,17 @@ package uk.ac.ucl.comp0010;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot application entry point.
+ */
 @SpringBootApplication
 public class Group007Application {
+
+  /**
+   * The main entry point.
+   *
+   * @param args command-line arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(Group007Application.class, args);
   }
