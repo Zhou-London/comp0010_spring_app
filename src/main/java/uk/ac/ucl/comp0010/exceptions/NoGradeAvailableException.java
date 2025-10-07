d925b9b2ba39a6ea9d3c41077359ed30cf396473
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-07 22:49:11 +0100

    Creates Exceptions.

---
+++ src/main/java/uk/ac/ucl/comp0010/exceptions/NoGradeAvailableException.java
@@ -0,0 +1,8 @@
+package uk.ac.ucl.comp0010.exceptions;
+
+/***
+ * Thrown by Student class.
+ */
+public class NoGradeAvailableException {
+
+}
---
+++ src/main/java/uk/ac/ucl/comp0010/exceptions/NoRegistrationException.java
@@ -0,0 +1,8 @@
+package uk.ac.ucl.comp0010.exceptions;
+
+/**
+ * Thrown by Student Class.
+ */
+public class NoRegistrationException {
+
+}
