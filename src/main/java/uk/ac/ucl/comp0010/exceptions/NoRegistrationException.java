467d67dcf5e19022eb73d26331bdb535151858ec
Rain Zhao <zcabrz1@ucl.ac.uk>
2025-10-09 20:00:03 +0100

    Add Registration and Exception skeletons with test files

--- src/main/java/uk/ac/ucl/comp0010/exceptions/NoRegistrationException.java
+++ src/main/java/uk/ac/ucl/comp0010/exceptions/NoRegistrationException.java
@@ -3,6 +3,8 @@ package uk.ac.ucl.comp0010.exceptions;
 /**
  * Thrown by Student Class.
  */
-public class NoRegistrationException {
-
+public class NoRegistrationException extends Exception {
+  public NoRegistrationException(String message) {
+    super(message);
+  }
 }
--- src/test/java/uk/ac/ucl/comp0010/Group007ApplicationTests.java
+++ src/test/java/uk/ac/ucl/comp0010/Group007ApplicationTests.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,3 +25,4 @@ class Group007ApplicationTests {
 				.andExpect(jsonPath("$.score").value(50));
 	}
 }
+
