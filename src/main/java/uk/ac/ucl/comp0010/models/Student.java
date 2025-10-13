8ca8d04b1be16bdd693f34257732e0fce7218ee2
Junwei Liang <a13316953229@gmail.com>
2025-10-13 03:04:10 +0100

    Updates properties

--- src/main/java/uk/ac/ucl/comp0010/models/Student.java
+++ src/main/java/uk/ac/ucl/comp0010/models/Student.java
@@ -18,10 +18,8 @@ public class Student {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "first_name", nullable = false)
   private String firstName;
 
-  @Column(name = "last_name", nullable = false)
   private String lastName;
 
   @Column(unique = true, nullable = false)
