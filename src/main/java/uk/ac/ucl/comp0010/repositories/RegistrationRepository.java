d9b9df16ccf1065f233b1cc4c8b8fbc549932e35
Junwei Liang <a13316953229@gmail.com>
2025-10-16 15:07:51 +0100

    Implements RegistrationRepository

---
+++ src/main/java/uk/ac/ucl/comp0010/repositories/RegistrationRepository.java
@@ -0,0 +1,15 @@
+package uk.ac.ucl.comp0010.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uk.ac.ucl.comp0010.models.Registration;
+
+/**
+ * Repository for Registration.
+ *
+ * @author YUNQ
+ */
+@Repository
+public interface RegistrationRepository extends CrudRepository<Registration, Long> {
+}
