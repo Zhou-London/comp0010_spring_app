abcc45d20e00e26db544d64b055148f0de54f507
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-23 14:06:07 +0100

    Fixs deps.

--- src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
+++ src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
@@ -1,8 +1,6 @@
 package uk.ac.ucl.comp0010.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.ac.ucl.comp0010.models.Grade;
 
 /**
  * Controller for Grade.
