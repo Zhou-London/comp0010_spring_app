566fab3934688d96d25979b74274fe354be586ec
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-23 13:16:35 +0100

    Remove grade controller.

--- src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
+++ src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
@@ -1,24 +1,9 @@
 package uk.ac.ucl.comp0010.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.ac.ucl.comp0010.models.Grade;
-import uk.ac.ucl.comp0010.repositories.StudentRepository;
 
 /**
  * Controller for Grade.
  */
 @RestController
 public class GradeController {
-
-
-  /**
-   * GET for "/grade" to get a Grade JSON.
-   *
-   * @return Return a Grade JSON.
-   */
-  @GetMapping("/grade")
-  public Grade getGrade() {
-    return new Grade(50);
-  }
 }
