51b4728556f1e6fc9cfe8b8bbed617602033a9a5
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-23 13:16:27 +0100

    Improves the main entrance.

--- src/main/java/uk/ac/ucl/comp0010/Group007Application.java
+++ src/main/java/uk/ac/ucl/comp0010/Group007Application.java
@@ -3,31 +3,24 @@ package uk.ac.ucl.comp0010;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import uk.ac.ucl.comp0010.models.Student;
-import uk.ac.ucl.comp0010.repositories.StudentRepository;
 
 /**
  * Spring Boot application entry point.
  */
 @SpringBootApplication
 public class Group007Application implements CommandLineRunner {
-  private final StudentRepository studentRepo;
 
-  public Group007Application(StudentRepository studentRepo) {
-    this.studentRepo = studentRepo;
-  }
   /**
    * The main entry point.
    *
    * @param args command-line arguments
    */
-
   public static void main(String[] args) {
     SpringApplication.run(Group007Application.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    studentRepo.save(new Student("Mari", "Ohara", "oharamari", "awashimaisland@qq.com"));
+    System.out.println("Application Initialized");
   }
 }
