49027c0be9c3fa7e81de467a1089caa2121f81d4
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-23 13:51:59 +0100

    Removes CW and fixs deps

--- src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
+++ src/main/java/uk/ac/ucl/comp0010/controllers/GradeController.java
@@ -1,9 +1,13 @@
 package uk.ac.ucl.comp0010.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ucl.comp0010.models.Grade;
 
 /**
  * Controller for Grade.
  */
 @RestController
 public class GradeController {
+
 }
