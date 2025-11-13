dbda8d23e4ef28849d9b500290bf2cc1f9d00dc8
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-11-13 15:52:01 +0000

    Implements test case for students API.

--- src/test/java/uk/ac/ucl/comp0010/Group007ApplicationTests.java
+++ src/test/java/uk/ac/ucl/comp0010/Group007ApplicationTests.java
@@ -1,15 +1,18 @@
 package uk.ac.ucl.comp0010;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.ac.ucl.comp0010.models.Module;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import uk.ac.ucl.comp0010.repositories.ModuleRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,9 +22,55 @@ class Group007ApplicationTests {
   private MockMvc mockMvc;
 
   @Autowired
-  private ModuleRepository moduleRepository;
+  private ObjectMapper objectMapper;
 
+  /**
+   * Students Test: Create John.
+   *
+   * @throws Exception
+   */
   @Test
-  void contextLoads() {}
-}
+  void testPostStudent() throws Exception {
+    Map<String, Object> req = Map.of("firstName", "John", "lastName", "Doe", "userName", "johndoe",
+        "email", "hello@world.com", "password", "team007");
 
+    mockMvc.perform(post("/students").contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(req))).andExpect(status().isCreated());
+  }
+
+  /**
+   * Students Test: Get John and all students.
+   *
+   * @throws Exception
+   */
+  @Test
+  void testGetStudent() throws Exception {
+    mockMvc.perform(get("/students")).andExpect(status().isOk());
+
+    mockMvc.perform(get("/students/1")).andExpect(status().isOk())
+        .andExpect(jsonPath("$.userName").value("johndoe"));
+  }
+
+  /**
+   * Students Test: Update John and check.
+   *
+   * @throws Exception
+   */
+  @Test
+  void testPutStudent() throws Exception {
+    Map<String, Object> req = Map.of("firstName", "JohnNew", "lastName", "DoeNew", "userName",
+        "johndoeNew", "email", "hello@world.com", "password", "team007");
+
+    mockMvc.perform(put("/students/1").contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(req))).andExpect(status().isOk());
+
+    mockMvc.perform(get("/students/1")).andExpect(status().isOk())
+        .andExpect(jsonPath("$.userName").value("johndoeNew"));
+  }
+
+  /* Grade */
+  @Test
+  void testGetGrade() throws Exception {
+    mockMvc.perform(get("/grades")).andExpect(status().isOk());
+  }
+}
